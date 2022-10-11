289777a1dd4abf67b116c5aea7ccd1635e149eee
Tyler French <tylerfrench89@gmail.com>
2022-10-10 20:54:35 -0700

    log

--- script.js
+++ script.js
@@ -1,91 +1,77 @@
 //IIFE for the game itself
 (function () {
     "use strict";
-    //module for the game board
-    const gameBoardModule = (() => {
-      const gameBoard = document.getElementById("game-board");
   
+    //module for the game board
+    const gameBoard = (() => {
+      const squares = document.querySelectorAll(".square");
       const board = [
         ["x", "", ""],
         ["", "", ""],
         ["", "", "o"],
       ];
   
-      //create the board
-      const renderBoard = () => {
-        for (let i = 0; i < board.length; i++) {
-          for (let k = 0; k < board[i].length; k++) {
-            const square = document.createElement("div");
-            square.classList.add("square");
-            square.innerText = board[i][k];
-            console.log(board[i][k])
-            gameBoard.appendChild(square);
+  
+  
+      //function for rendering move
+      const renderMove = (g) => {
+        for(let i = 0; i < board.length; i++) {
+          for(let j = 0; j < board.length; j++) {
+            console.log(board[i][j])
           }
         }
-        //attach the event handlers
-        const squares = document.querySelectorAll(".square");
-        for (let i = 0; i < squares.length; i++) {
-          (() => {
-            squares[i].dataset.dataIndex = i;
-            squares[i].addEventListener(
-              "click",
-              controller.clickHandler.bind(squares[i])
-            );
-          })();
-        }
+        // console.log('hey', g)
       };
   
-      return { board, renderBoard };
+      squares.forEach((sqaure, index) => {
+        sqaure.addEventListener("click", renderMove.bind(this, index));
+      });
+  
+      return { renderMove };
     })();
   
     //module for the controller
-    const controller = (() => {
-      const resetBtn = document.getElementById("reset-btn");
-      const gameOver = false;
-      let moves = 1; 
-      let currentPlayer = 1;
+    // const controller = (() => {
+    //   const resetBtn = document.getElementById("reset-btn");
+    //   const gameOver = false;
+    //   let currentPlayer = 1;
   
-      function clickHandler() {
-        if (this.innerText != "x" && this.innerText != "o") {
-          if (currentPlayer === 2) {
-            this.innerText = "o";
-            currentPlayer = 1;
-          } else if (currentPlayer === 1) {
-            this.innerText = "x";
-            currentPlayer = 2;
-            
-          }
-          moves++
-          checkforWin()
-        }
-      }
-      function checkforWin () {
-        
-      }
-   
-      const reset = () => {
-        const squares = document.querySelectorAll(".square");
-        squares.forEach((square) => {
-          square.innerText = "";
-        });
-      };
+    //   const clickHandler = () => {
+    //     if (this.innerText != "x" && this.innerText != "o") {
+    //       if (currentPlayer === 2) {
+    //         this.innerText = "o";
+    //         currentPlayer = 1;
+    //       } else if (currentPlayer === 1) {
+    //         this.innerText = "x";
+    //         currentPlayer = 2;
   
-      resetBtn.addEventListener("click", reset);
+    //       }
+    //       moves++
+    //       checkforWin()
+    //     }
+    //   }
   
-      return { gameOver, clickHandler };
-    })();
+    //   const reset = () => {
+    //     const squares = document.querySelectorAll(".square");
+    //     squares.forEach((square) => {
+    //       square.innerText = "";
+    //     });
+    //   };
   
-    const Player = (name, score) => {
+    //   resetBtn.addEventListener("click", reset);
+  
+    //   return { gameOver, currentPlayer, clickHandler };
+    // })();
+  
+    const Player = (name, score, sign) => {
       const getName = () => name;
       const getScore = () => score;
+      const getSign = () => sign;
   
-      const makeMove = (t) => console.log("yeee", t);
-  
-      return { getName, getScore, makeMove };
+      return { getName, getScore, getSign };
     };
   
     const player1 = Player("Tyler", "2", "x");
     const player2 = Player("Craig", "2", "o");
-    gameBoardModule.renderBoard(gameBoardModule.board);
   })();
   
