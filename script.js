94764abaa0f439754eb51ccb79919adf9d109e74
Tyler French <tylerfrench89@gmail.com>
2022-11-08 13:23:14 -0800

    link to github added to bottom of page

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 const player = (name, score, sign) => {
   const makeMove = (square, index) => {
-    if (square.innerText != "") {
-      return;
+    if (square.innerText != '') {
+      return
     }
-    gameBoard.render(index, sign);
-  };
+    gameBoard.render(index, sign)
+  }
 
-  return { name, score, sign, makeMove };
-};
+  return { name, score, sign, makeMove }
+}
 
 const gameBoard = (() => {
-  const squares = document.querySelectorAll(".square");
-  const boardMoves = new Array(9);
+  const squares = document.querySelectorAll('.square')
+  const boardMoves = new Array(9)
 
   function render(index, sign) {
-    changeColor(index, sign);
-    boardMoves[index] = sign;
-    squares[index].innerText = boardMoves[index];
-    controller.moves++;
-    console.log(controller.moves);
+    changeColor(index, sign)
+    boardMoves[index] = sign
+    squares[index].innerText = boardMoves[index]
+    controller.moves++
+    console.log(controller.moves)
     if (controller.checkForWin(sign)) {
-      controller.gameOver(sign);
-      return;
+      controller.gameOver(sign)
+      return
     } else if (controller.moves === 9) {
-      controller.gameOver();
+      controller.gameOver()
     }
-    controller.changePlayer();
+    controller.changePlayer()
   }
 
   function changeColor(index, sign) {
-    if (sign === "x") {
-      squares[index].setAttribute("style", "color: blue");
+    if (sign === 'x') {
+      squares[index].setAttribute('style', 'color: blue')
     } else {
-      squares[index].setAttribute("style", "color: red");
+      squares[index].setAttribute('style', 'color: red')
     }
   }
 
-  return { boardMoves, squares, render };
-})();
+  return { boardMoves, squares, render }
+})()
 
 const controller = (() => {
-  const resetBtn = document.getElementById("reset-btn");
-  const player1 = player("tyler", "2", "x");
-  const player2 = player("craig", "5", "o");
-  let currentPlayer = player1;
-  let moves = 0;
+  const resetBtn = document.getElementById('reset-btn')
+  const player1 = player('tyler', '2', 'x')
+  const player2 = player('craig', '5', 'o')
+  let currentPlayer = player1
+  let moves = 0
   const WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -54,49 +54,54 @@ const controller = (() => {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
+  ]
 
   function changePlayer() {
     if (currentPlayer === player1) {
-      currentPlayer = player2;
+      currentPlayer = player2
     } else if (currentPlayer === player2) {
-      currentPlayer = player1;
+      currentPlayer = player1
     }
   }
 
   const checkForWin = (player) => {
     return WINNING_COMBINATIONS.some((winCombo) => {
-      return winCombo.every((i) => gameBoard.boardMoves[i] === player);
-    });
-  };
+      return winCombo.every((i) => gameBoard.boardMoves[i] === player)
+    })
+  }
 
   const gameOver = (sign) => {
+    let over = false
     if (sign) {
-      console.log("game over " + sign + " wins");
+      console.log('game over ' + sign + ' wins')
+      over = true
     } else {
-      console.log("draw!");
+      console.log('draw!')
+      over = true
     }
-  };
+    return over
+  }
 
   const reset = () => {
-    controller.moves = 0;
-    const movesMade = gameBoard.boardMoves;
-    currentPlayer = player1;
+    controller.moves = 0
+    const movesMade = gameBoard.boardMoves
+    currentPlayer = player1
     for (let i = 0; i < movesMade.length; i++) {
-      movesMade[i] = "";
-      gameBoard.squares[i].innerText = "";
+      movesMade[i] = ''
+      gameBoard.squares[i].innerText = ''
     }
-  };
+  }
 
   gameBoard.squares.forEach((square, index) => {
-    square.addEventListener("click", () => {
-      currentPlayer.makeMove(square, index);
-    });
-  });
+    square.addEventListener('click', () => {
+      
+      currentPlayer.makeMove(square, index)
+    })
+  })
 
-  resetBtn.addEventListener("click", () => {
-    reset();
-  });
+  resetBtn.addEventListener('click', () => {
+    reset()
+  })
 
-  return { currentPlayer, changePlayer, checkForWin, gameOver, moves };
-})();
+  return { currentPlayer, changePlayer, checkForWin, gameOver, moves }
+})()
